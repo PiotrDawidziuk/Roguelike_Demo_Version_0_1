230d9f9640501138a2887bd036535a72141d8385
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2022-05-18 14:41:34 +0200

    key added, label fixed, points added. no save

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -1,24 +1,32 @@
 extends Node2D
 
 
-const Player = preload("res://Scenes/hero.tscn")
+#const Player = preload("res://Scenes/hero.tscn")
 const Secret = preload("res://Scenes/secret.tscn")
 const Exit = preload("res://Scenes/level_exit.tscn")
 const WALKERS_STEPS = 200
 
 #TEST VARIABLES
-var cell_sample
-onready var test_label = $Label
+var cell_sample # not needed
+
+#needed variables
 var cell_table = []
 var cell_dict = {}
 var secret = Secret.instance()
 var exit = Exit.instance()
 
+#variables of things player has etc:
+var hasKey = false
+var points = 0
+
 var borders = Rect2(1,1, 38, 21)
 
 onready var tileMap = $TileMap
 
-var player = Player.instance()
+onready var player = $hero
+onready var message_box = $hero/Label
+onready var points_label = $hero/PointsLabel
+onready var key = $key
 
 
 func _ready():
@@ -28,7 +36,7 @@ func _ready():
 func generate_level():
 	var walker = Walker.new(Vector2(19,11), borders)
 	var map = walker.walk(WALKERS_STEPS)
-	add_child(player)
+	#add_child(player)
 	var initial_position = map.front()*32
 	player.position = initial_position
 	walker.queue_free()
@@ -48,6 +56,8 @@ func generate_level():
 	#exit.position = cell_table[40].position*32
 	exit.position = walker.get_end_room().position*32
 	
+	key.position = cell_table[20].position*32
+	
 	tileMap.set_cellv(Vector2(1,1), -1)
 	
 	cell_sample = MapClasses.MapCellProperties.new()
@@ -55,8 +65,8 @@ func generate_level():
 	tileMap.update_bitmask_region(borders.position, borders.end)
 	
 	cell_sample = tileMap.get_cell(2,2)
-	# test_label.text = String(cell_sample)
-	test_label.text = "Welcome Adventurer!"
+	# message_box.text = String(cell_sample)
+	message_box.text = "Welcome Adventurer!"
 
 func reaload_level():
 	get_tree().reload_current_scene()
@@ -67,29 +77,38 @@ func _input(event):
 	if event.is_action_pressed("ui_left"):
 		if cell_dict.has(Vector2((player.position.x / 32) -1, player.position.y / 32)):
 			var test_text_1 = String(cell_dict[Vector2(player.position.x / 32, player.position.y / 32)].position) 
-			test_label.text = "You went West from " + test_text_1
+			message_box.text = "You went West from " + test_text_1
 			player.position = Vector2(player.position.x -32, player.position.y)
 	if event.is_action_pressed("ui_right"):
 		if cell_dict.has(Vector2((player.position.x / 32) +1, player.position.y / 32)):
 			var test_text_1 = String(cell_dict[Vector2(player.position.x / 32, player.position.y / 32)].position) 
-			test_label.text = "You went East from " + test_text_1
+			message_box.text = "You went East from " + test_text_1
 			player.position = Vector2(player.position.x +32, player.position.y)
 	if event.is_action_pressed("ui_up"):
 		if cell_dict.has(Vector2(player.position.x / 32, (player.position.y / 32)-1)):
 			var test_text_1 = String(cell_dict[Vector2(player.position.x / 32, player.position.y / 32)].position) 
-			test_label.text = "You went North from " + test_text_1
+			message_box.text = "You went North from " + test_text_1
 			player.position = Vector2(player.position.x, player.position.y - 32)
 	if event.is_action_pressed("ui_down"):
 		if cell_dict.has(Vector2(player.position.x / 32, (player.position.y / 32)+1)):
 			var test_text_1 = String(cell_dict[Vector2(player.position.x / 32, player.position.y / 32)].position) 
-			test_label.text = "You went South from" + test_text_1
+			message_box.text = "You went South from" + test_text_1
 			player.position = Vector2(player.position.x, player.position.y + 32)
 	meeting(player.position)
 
 func meeting(position):
 	if secret != null && position == secret.position:
-		test_label.text = "You found something!"
+		message_box.text = "You found something!"
+		points = points + 1
+		points_label.text = "Points: " + String(points)
 		secret.queue_free()
+	if key != null && position == key.position:
+		message_box.text = "You found a key!"
+		hasKey = true
+		key.queue_free()	
 	if exit != null && position == exit.position:
-		test_label.text = "You exit level!"
-		reaload_level()
+		if (hasKey):
+			reaload_level()
+		else:
+			message_box.text = "The exit is locked!"
+		
